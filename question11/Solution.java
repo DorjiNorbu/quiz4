ccd9b093c33a906701882facb95c15cebbfa8739
Dorji Norbu <dnorbu33@gmail.com>
2020-08-26 00:01:38 +0600

    modified some lines

--- question11/Solution.java
+++ question11/Solution.java
@@ -4,30 +4,15 @@ package quiz4.question11;
  * Created by Dorji Norbu on 25-Aug-20.
  */
 public class Solution {
-    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-//        int k = 0;
-//        int count = arr1.length;//assume all elements are valid
-//        for(int m = 0; m < arr2.length && k< arr1.length; m++){
-//
-//            if(Math.abs(arr1[k] - arr2[m]) <= d) {
-//                k++;
-//                m = -1;
-//                count--;//take off the invalid element
-//            }
-//            if(m== arr2.length-1){//in case the prior element is valid
-//                k++;
-//                m = -1;
-//            }
-//        }
-//        return count;
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int dist) {
         int distance = arr1.length;
 
-        outer:
+        outerFor:
         for (int n1 : arr1) {
-            for(int n2 : arr2) {
-                if(Math.abs(n1 - n2) <= d) {
+            for (int n2 : arr2) {
+                if (Math.abs(n1 - n2) <= dist) {
                     distance--;
-                    continue outer;
+                    continue outerFor;
                 }
             }
         }
--- question13/QuestionThirteen.java
+++ question13/QuestionThirteen.java
@@ -6,9 +6,9 @@ package quiz4.question13;
 public class QuestionThirteen {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int array[]={1,2,2,1,1,3};
+        int array[]={1,2,2,1,1,3};
 //        int array[]={1,2};
-        int array[]={-3,0,1,-3,1,1,1,-3,10,0};
+//        int array[]={-3,0,1,-3,1,1,1,-3,10,0};
         System.out.println(solution.uniqueOccurrence(array));
     }
 }
